b767cc58f2d9a062eac4a1739d562680e0fb7f05
Septian Rahmad <wseptianrahmad@gmail.com>
2026-07-25 20:35:19 +0700

    Tambah security headers middleware (CSP, HSTS support, dll)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'admin' => AdminMiddleware::class,
             'operator' => OperatorMiddleware::class,
         ]);
+        $middleware->append(\App\Http\Middleware\SecurityHeaders::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
